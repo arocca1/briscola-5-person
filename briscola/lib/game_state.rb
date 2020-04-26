54c45e0f9596562226fd856f4dcbddea8212f74a
antonio <rocca1.antonio@gmail.com>
2020-04-26 11:25:22 -0400

    lib/game_state: Continue building up game state from player perspective
    The remaining piece here is that I need to identify how to record
    the current hand

--- briscola/lib/game_state.rb
+++ briscola/lib/game_state.rb
@@ -17,12 +17,25 @@ module GameState
     }
     =end
     state = { id: player.id }
-    state[:my_partners] = player.partners.pluck(:partner_id)
+    my_partners = player.partners.pluck(:partner_id)
+    state[:my_partners] = my_partners.map(&:to_s) # JavaScript is a butt
     state[:my_cards] = player.unplayed_cards.map do |pl_card|
       {
         name: pl_card.card.name,
         raw_value: pl_card.card.raw_value,
       }
+    end
+
+    # TODO figure out how to identify active hand
+
+    if active_game.requires_bidding
+      player_bid = active_game.player_active_game_bids.select(:player_id, :bid).where("bid = (#{active_game.player_active_game_bids.select('MAX(bid)').to_sql})")
+      state[:highest_bid] = player_bid.bid
+      state[:highest_bidder] = player_bid.player_id.to_s # JavaScript is a butt
+    end
+
+    if show_score
+      state[:my_team_score] = active_game.hands.where(winner_id: my_partners + [player.id]).sum(:score)
     end
 
     state
