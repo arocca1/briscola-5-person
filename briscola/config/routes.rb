b1002447982cb717a64c3bf21de3c24dcc8ddef1
antonio <rocca1.antonio@gmail.com>
2020-04-26 20:25:34 -0400

    Add missing routes for all controllers

--- briscola/config/routes.rb
+++ briscola/config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   resources :games, only: [:create, :index, :show] do
+    collection do
+      post 'join'
+    end
   end
   resources :bids do
     collection do
       post 'make_bid'
       post 'pass_bid'
+      post 'set_partner_card'
+    end
+  end
+  resources :play do
+    collection do
+      post 'deal_cards'
+      post 'play_card'
     end
   end
 end
