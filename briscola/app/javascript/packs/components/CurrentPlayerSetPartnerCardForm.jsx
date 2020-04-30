61070bcf27a47f0f98b487fdda6be64a26ac531b
antonio <rocca1.antonio@gmail.com>
2020-04-30 17:37:22 -0400

    CurrentPlayerSetPartnerCardForm: Get the partner form to render
    The dropdowns are weird, but it's okay for now

--- briscola/app/javascript/packs/components/CurrentPlayerSetPartnerCardForm.jsx
+++ briscola/app/javascript/packs/components/CurrentPlayerSetPartnerCardForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 const CurrentPlayerSetPartnerCardForm = props => {
   if (!props.gameState.bidding_done || props.gameState.partner_card || !props.gameState.suits || !props.gameState.raw_values) {
@@ -9,21 +10,31 @@ const CurrentPlayerSetPartnerCardForm = props => {
   }
 
   const suitValues = props.gameState.suits.map(suit => {
-    <Dropdown.Item key={`${suit.name}Key`} eventKey={suit.id}>{ suit.name }</Dropdown.Item>
+    return <Dropdown.Item key={`${suit.name}Key`} eventKey={suit.id}>{ suit.name }</Dropdown.Item>
   })
   const suitValue = props.gameState.suits.find(elem => elem.id === props.partnerSuitId);
   const suitDropdown = (
-    <DropdownButton key="SuitDropdownKey" onChange={props.handleSetPartnerSuit} title={`${suitValue && suitValue.name || "Suit"}`}>
+    <DropdownButton
+      key="SuitDropdownKey"
+      variant="secondary"
+      onChange={props.handleSetPartnerSuit}
+      title={`${suitValue && suitValue.name || "Suit"}`}
+    >
       { suitValues }
     </DropdownButton>
   );
 
   const rawValues = props.gameState.raw_values.map(rawValue => {
-    <Dropdown.Item key={`${rawValue.name}Key`} eventKey={rawValue.raw_value}>{ rawValue.name }</Dropdown.Item>
+    return <Dropdown.Item key={`${rawValue.name}Key`} eventKey={rawValue.raw_value}>{ rawValue.name }</Dropdown.Item>
   })
   const partnerRawValue = props.gameState.raw_values.find(elem => elem.raw_value === props.partnerRawValue);
   const rawValueDropdown = (
-    <DropdownButton key="SuitDropdownKey" onChange={props.handleSetPartnerRawValue} title={`${partnerRawValue && partnerRawValue.name || "Value"}`}>
+    <DropdownButton
+      key="SuitDropdownKey"
+      variant="secondary"
+      onChange={props.handleSetPartnerRawValue}
+      title={`${partnerRawValue && partnerRawValue.name || "Value"}`}
+    >
       { rawValues }
     </DropdownButton>
   );
